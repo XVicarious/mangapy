a936cb3dd403cff816887d8d50caa604a1d19929
Brian Maurer <bmaurer@xvicario.us>
2020-03-30 17:05:49 -0400

    some comments

--- mangapy/crawler.py
+++ mangapy/crawler.py
@@ -13,12 +13,12 @@ def library_crawler(library_path: Path):
     library = DB.session.query(Library).filter(Library.path == str(library_path)).first()
     for series in os.listdir(library_path):
         manga = DB.session.query(Manga).filter(Manga.path == series).first()
-        if not manga:
+        if not manga:  # if the manga doesn't exist, create it
             manga = Manga(series)
-            library.manga.extend([manga])
+            library.manga.extend([manga])  # add it to the library
         for chapter in os.listdir(library_path / series):
             chap = DB.session.query(Chapter).filter(Chapter.path == chapter, Chapter.manga == manga).first()
-            if chap:
+            if chap:  # if the chapter already exists, continue on
                 continue
             chap = Chapter(chapter)
             manga.chapters.extend([chap])
@@ -30,5 +30,5 @@ def just_list(library_path):
         print(chapter.path, chapter.number)
 
 
-library_crawler(Path(lib_path))
+# library_crawler(Path(lib_path))
 # just_list(Path(lib_path))
